66530d22c65461235a899e6e876ba48bae63b76e
haj amir <amirfattahi22@gmail.com>
2024-05-23 15:20:23 +0330

    good

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,17 +1,13 @@
-import { ReactNode } from "react";
+import {PropsWithChildren} from "react";
 
-type CourseGoalProps = {
-  title: string;
-  description: string;
-  children: ReactNode;
-};
-export default function CourseGoal(props: CourseGoalProps) {
+type CourseGoalProps = PropsWithChildren<{title: string, description:string}>
+export default function CourseGoal({title, children, description}:CourseGoalProps) {
   return (
     <article>
       <div>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        {props.children}
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {children}
       </div>
       <button>delete</button>
     </article>
